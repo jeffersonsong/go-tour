ff350750d3ee9dc1e852f5b55c8ee314e3157e4e
Jefferson Song <jeffersonsong@yahoo.com>
2023-06-29 15:15:13 -0400

    Improve comments.

--- methods/interfaces-are-satified-implicitly.go
+++ methods/interfaces-are-satified-implicitly.go
@@ -10,7 +10,7 @@ type T struct {
 	S string
 }
 
-// This method means type T implements the interface I,
+// M This method means type T implements the interface I,
 // but we don't need to explicitly declare that it does so.
 func (t T) M() {
 	fmt.Println(t.S)
